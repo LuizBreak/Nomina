f8274d7b58cf2e0713fe02ff12328ecfe79be561
Luiz Break <luiz.break@gmail.com>
2021-11-16 03:38:30 -0400

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var selectedRow = null
 
+
 function onFormSubmit() {
+
     if (validate()) {
         var formData = readFormData();
         if (selectedRow == null)
-            insertNewRecord(formData);
+            fetchApiData2()
+            //insertNewRecord(formData);
         else
             updateRecord(formData);
         resetForm();
@@ -13,49 +16,60 @@ function onFormSubmit() {
 
 function readFormData() {
     var formData = {};
-    formData["fullName"] = document.getElementById("fullName").value;
-    formData["empCode"] = document.getElementById("empCode").value;
-    formData["salary"] = document.getElementById("salary").value;
-    formData["city"] = document.getElementById("city").value;
+    formData["nombre"] = document.getElementById("nombre").value;
+    formData["cedula"] = document.getElementById("cedula").value;
+    formData["concepto"] = document.getElementById("concepto").value;
+    formData["localidad"] = document.getElementById("localidad").value;
     return formData;
 }
 
-function insertNewRecord(data) {
-    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.fullName;
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.empCode;
-    cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.salary;
-    cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.city;
-    cell4 = newRow.insertCell(4);
-    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
-                       <a onClick="onDelete(this)">Delete</a>`;
+function insertNewRecord(element) {
+    
+    var table = document.getElementById("informe").getElementsByTagName('tbody')[0];
+    
+
+        var newRow = table.insertRow(table.length);
+
+        cell1 = newRow.insertCell(0);
+        cell1.innerHTML = element.fromUser;
+        cell2 = newRow.insertCell(1);
+        cell2.innerHTML = element.transactionDate;
+        cell3 = newRow.insertCell(2);
+        cell3.innerHTML = element.transactionType;
+        cell4 = newRow.insertCell(3);
+        cell4.innerHTML = element.locationName;
+        cell4 = newRow.insertCell(4);
+        cell4.innerHTML = element.amount;
+        cell5 = newRow.insertCell(5);
+        cell5.innerHTML = element.amount;
+        cell6 = newRow.insertCell(6);
+        cell6.innerHTML = element.amount;
+        cell7 = newRow.insertCell(7);
+        cell7.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                        <a onClick="onDelete(this)">Delete</a>`;
+
 }
 
 function resetForm() {
-    document.getElementById("fullName").value = "";
-    document.getElementById("empCode").value = "";
-    document.getElementById("salary").value = "";
-    document.getElementById("city").value = "";
+    document.getElementById("nombre").value = "";
+    document.getElementById("cedula").value = "";
+    document.getElementById("concepto").value = "";
+    document.getElementById("localidad").value = "";
     selectedRow = null;
 }
 
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("empCode").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("nombre").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("cedula").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("concepto").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("localidad").value = selectedRow.cells[3].innerHTML;
 }
 function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.fullName;
-    selectedRow.cells[1].innerHTML = formData.empCode;
-    selectedRow.cells[2].innerHTML = formData.salary;
-    selectedRow.cells[3].innerHTML = formData.city;
+    selectedRow.cells[0].innerHTML = formData.nombre;
+    selectedRow.cells[1].innerHTML = formData.cedula;
+    selectedRow.cells[2].innerHTML = formData.Concepto;
+    selectedRow.cells[3].innerHTML = formData.Localidad;
 }
 
 function onDelete(td) {
@@ -67,13 +81,71 @@ function onDelete(td) {
 }
 function validate() {
     isValid = true;
-    if (document.getElementById("fullName").value == "") {
+    if (document.getElementById("nombre").value == "") {
         isValid = false;
-        document.getElementById("fullNameValidationError").classList.remove("hide");
+        document.getElementById("nombreValidationError").classList.remove("hide");
     } else {
         isValid = true;
-        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
-            document.getElementById("fullNameValidationError").classList.add("hide");
+        if (!document.getElementById("nombreValidationError").classList.contains("hide"))
+            document.getElementById("nombreValidationError").classList.add("hide");
     }
     return isValid;
 }
+
+function fetchApiData2(){
+    
+    // Article Reference: https://www.digitalocean.com/community/tutorials/how-to-use-the-javascript-fetch-api-to-get-data
+
+    const url = 'https://d51wibckd0.execute-api.us-east-1.amazonaws.com/dev/items';
+    var done = false;
+
+    fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data) {
+    let authors = data.Items;
+    return authors.map(function(author) {
+
+        insertNewRecord(author);
+
+        })
+    })
+    .catch(function(error) {
+    console.log(error);
+    });
+}
+
+function postApiData2(){
+
+    // Article Reference: https://stackabuse.com/using-fetch-to-send-http-requests-in-javascript/
+
+    const url = 'https://w2edkzxi8h.execute-api.us-east-1.amazonaws.com/common/api/v1/transactions';
+
+    let data = {
+        "fromUser": "source-user-Z",
+        "toUser": "test-user",
+        "timestamp": 1733651606459,
+        "transactionDate": "2021-10-07 20:06:46.459",
+        "transactionType": "xPayment",
+        "userId": 1,
+        "locationName": "xPlaza de la Bandera",
+        "description": "xJardin Botanico",
+        "amount": 16.29,
+        "longitude": 0,
+        "latitude": 0
+    }
+
+    var request = new Request(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
+    });
+
+    fetch(request)
+    .then(response => response.json())
+    .then(json => {
+        console.log(json);
+    });
+}
+
