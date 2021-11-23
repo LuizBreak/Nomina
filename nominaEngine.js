b724e398553bad3a51e6c886c6141ae9b3f234b6
Arielle <arielle2madet@gmail.com>
2021-11-23 16:11:29 -0400

    Columnas reposicionadas

--- nominaEngine.js
+++ nominaEngine.js
@@ -52,57 +52,58 @@ function refreshNominaReport(element) {
         var newRow = table.insertRow(table.length);
 
         cell1 = newRow.insertCell(0);
-        cell1.innerHTML = element.nombre;
-        cell1.setAttribute("data-label", "Nombre");
-
-        cell2 = newRow.insertCell(1);
-        cell2.innerHTML = element.cedula;
-        cell2.setAttribute("data-label", "Cedula");
-        cell3 = newRow.insertCell(2);
-        cell3.innerHTML = element.concepto;
-        cell3.setAttribute("data-label", "Concepto");
-        cell4 = newRow.insertCell(3);
-        cell4.innerHTML = element.Localidad;
-        cell4.setAttribute("data-label", "Localidad");
-        cell5 = newRow.insertCell(4);
-        cell5.innerHTML = element.personaCubierta;
-        cell5.setAttribute("data-label", "Persona Cubierta");
-        cell6 = newRow.insertCell(5);
-        cell6.innerHTML = element.diasCobertura;
-        cell6.setAttribute("data-label", "Dias de cobertura");
-        cell7 = newRow.insertCell(6);
-        cell7.innerHTML = element.mesCobertura;
-        cell7.setAttribute("data-label", "Mes de cobertura");
-        cell8 = newRow.insertCell(7);
-        cell8.innerHTML = element.horaEntrada;
-        cell8.setAttribute("data-label", "Hora de entrada");
-        cell9 = newRow.insertCell(8);
-        cell9.innerHTML = element.horaSalida;
-        cell9.setAttribute("data-label", "Hora de salida");
-        cell10 = newRow.insertCell(9);
-        cell10.innerHTML = element.horaAlmuerzo;
-        cell10.setAttribute("data-label", "Mantener hora de almuerzo");
-        cell11 = newRow.insertCell(10);
-        cell11.innerHTML = element.montosNegociados;
-        cell11.setAttribute("data-label", "Montos Negociados");
-        cell12 = newRow.insertCell(11);
-        cell12.innerHTML = element.comentariosAdicionales;
-        cell12.setAttribute("data-label", "Comentarios Adicionales");
-        cell13 = newRow.insertCell(12);
-        cell13.innerHTML = element.evidencia;
-        cell13.setAttribute("data-label", "Evidencia");
-        cell14 = newRow.insertCell(13);
-        cell14.innerHTML = element.supervisor;
-        cell14.setAttribute("data-label", "Supervisor");
-        cell15 = newRow.insertCell(14);
-        cell15.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+        cell1.innerHTML = `<a onClick="onEdit(this)">Edit</a>
                             <a onClick="onDelete(this)">Delete</a>`;
 
-        cell15.setAttribute("data-label", "Action");
+        cell1.setAttribute("data-label", "Action");
         
-        cell10 = newRow.insertCell(15);
-        cell10.innerHTML = element.timestamp;
-        cell10.setAttribute("data-label", "Timestamp");
+        cell2 = newRow.insertCell(1);
+        cell2.innerHTML = element.nombre;
+        cell2.setAttribute("data-label", "Nombre");
+
+        cell3 = newRow.insertCell(2);
+        cell3.innerHTML = element.cedula;
+        cell3.setAttribute("data-label", "Cedula");
+        cell4 = newRow.insertCell(3);
+        cell4.innerHTML = element.concepto;
+        cell4.setAttribute("data-label", "Concepto");
+        cell5 = newRow.insertCell(4);
+        cell5.innerHTML = element.Localidad;
+        cell5.setAttribute("data-label", "Localidad");
+        cell6 = newRow.insertCell(5);
+        cell6.innerHTML = element.personaCubierta;
+        cell6.setAttribute("data-label", "Persona Cubierta");
+        cell7 = newRow.insertCell(6);
+        cell7.innerHTML = element.diasCobertura;
+        cell7.setAttribute("data-label", "Dias de cobertura");
+        cell8 = newRow.insertCell(7);
+        cell8.innerHTML = element.mesCobertura;
+        cell8.setAttribute("data-label", "Mes de cobertura");
+        cell9 = newRow.insertCell(8);
+        cell9.innerHTML = element.horaEntrada;
+        cell9.setAttribute("data-label", "Hora de entrada");
+        cell10 = newRow.insertCell(9);
+        cell10.innerHTML = element.horaSalida;
+        cell10.setAttribute("data-label", "Hora de salida");
+        cell11 = newRow.insertCell(10);
+        cell11.innerHTML = element.horaAlmuerzo;
+        cell11.setAttribute("data-label", "Mantener hora de almuerzo");
+        cell12 = newRow.insertCell(11);
+        cell12.innerHTML = element.montosNegociados;
+        cell12.setAttribute("data-label", "Montos Negociados");
+        cell13 = newRow.insertCell(12);
+        cell13.innerHTML = element.comentariosAdicionales;
+        cell13.setAttribute("data-label", "Comentarios Adicionales");
+        cell14 = newRow.insertCell(13);
+        cell14.innerHTML = element.evidencia;
+        cell14.setAttribute("data-label", "Evidencia");
+        cell15 = newRow.insertCell(14);
+        cell15.innerHTML = element.supervisor;
+        cell15.setAttribute("data-label", "Supervisor");
+        
+        cell16 = newRow.insertCell(15);
+        cell16.innerHTML = element.timestamp;
+        cell16.setAttribute("data-label", "Timestamp");
 }
 
 function createForm(){
@@ -176,61 +177,62 @@ function onEdit(td) {
 
     selectedRow = td.parentElement.parentElement;
 
+    // [0] -> Action
+
     var objSelect = document.getElementById("ddNombreBkp-List");
-    let nombreValue = setSelectedValue(objSelect, selectedRow.cells[0].innerHTML);
+    let nombreValue = setSelectedValue(objSelect, selectedRow.cells[1].innerHTML);
     document.getElementById("ddNombreBkp").value = nombreValue;
 
-    document.getElementById("cedula").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("cedula").value = selectedRow.cells[2].innerHTML;
 
     var objSelect = document.getElementById("concepto");
-    setSelectedValue(objSelect, selectedRow.cells[2].innerHTML);
+    setSelectedValue(objSelect, selectedRow.cells[3].innerHTML);
 
-    document.getElementById("localidad").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("localidad").value = selectedRow.cells[4].innerHTML;
 
     var objSelect = document.getElementById("ddNombreCubierta-List");
-    let nombreCubiertavalue =  setSelectedValue(objSelect, selectedRow.cells[4].innerHTML);
+    let nombreCubiertavalue =  setSelectedValue(objSelect, selectedRow.cells[5].innerHTML);
     document.getElementById("ddNombreCubierta").value = nombreCubiertavalue;
 
-    document.getElementById("diasCobertura").value = selectedRow.cells[5].innerHTML;
-    document.getElementById("mesCobertura").value = selectedRow.cells[6].innerHTML;
-    document.getElementById("horaEntrada").value = selectedRow.cells[7].innerHTML;
-    document.getElementById("horaSalida").value = selectedRow.cells[8].innerHTML;
-    document.getElementById("horaAlmuerzo").value = selectedRow.cells[9].innerHTML;
-    document.getElementById("montosNegociados").value = selectedRow.cells[10].innerHTML;
-    document.getElementById("comentariosAdicionales").value = selectedRow.cells[11].innerHTML;
-    document.getElementById("evidencia").value = selectedRow.cells[12].innerHTML;
+    document.getElementById("diasCobertura").value = selectedRow.cells[6].innerHTML;
+    document.getElementById("mesCobertura").value = selectedRow.cells[7].innerHTML;
+    document.getElementById("horaEntrada").value = selectedRow.cells[8].innerHTML;
+    document.getElementById("horaSalida").value = selectedRow.cells[9].innerHTML;
+    document.getElementById("horaAlmuerzo").value = selectedRow.cells[10].innerHTML;
+    document.getElementById("montosNegociados").value = selectedRow.cells[11].innerHTML;
+    document.getElementById("comentariosAdicionales").value = selectedRow.cells[12].innerHTML;
+    document.getElementById("evidencia").value = selectedRow.cells[13].innerHTML;
 
     var objSelect = document.getElementById("ddSupervisor-List");
-    let supervisorvalue =  setSelectedValue(objSelect, selectedRow.cells[13].innerHTML);
+    let supervisorvalue =  setSelectedValue(objSelect, selectedRow.cells[14].innerHTML);
     document.getElementById("ddSupervisor").value = supervisorvalue;
 
-    // [14] -> Action
 
     document.getElementById("timestamp").value = selectedRow.cells[15].innerHTML;
 }
 function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.ddNombreBkp;
-    selectedRow.cells[1].innerHTML = formData.cedula;
-    selectedRow.cells[2].innerHTML = formData.concepto;
-    selectedRow.cells[3].innerHTML = formData.localidad;
-    selectedRow.cells[4].innerHTML = formData.ddNombreCubierta;
-    selectedRow.cells[5].innerHTML = formData.diasCobertura;
-    selectedRow.cells[6].innerHTML = formData.mesCobertura;
-    selectedRow.cells[7].innerHTML = formData.horaEntrada;
-    selectedRow.cells[8].innerHTML = formData.horaSalida;
-    selectedRow.cells[9].innerHTML = formData.horaAlmuerzo;
-    selectedRow.cells[10].innerHTML = formData.montosNegociados;
-    selectedRow.cells[11].innerHTML = formData.comentariosAdicionales;
-    selectedRow.cells[12].innerHTML = formData.evidencia;
-    selectedRow.cells[13].innerHTML = formData.ddSupervisor;
-    // [14] -> Action 
+    // [0] -> Action
+    selectedRow.cells[1].innerHTML = formData.ddNombreBkp;
+    selectedRow.cells[2].innerHTML = formData.cedula;
+    selectedRow.cells[3].innerHTML = formData.concepto;
+    selectedRow.cells[4].innerHTML = formData.localidad;
+    selectedRow.cells[5].innerHTML = formData.ddNombreCubierta;
+    selectedRow.cells[6].innerHTML = formData.diasCobertura;
+    selectedRow.cells[7].innerHTML = formData.mesCobertura;
+    selectedRow.cells[8].innerHTML = formData.horaEntrada;
+    selectedRow.cells[9].innerHTML = formData.horaSalida;
+    selectedRow.cells[10].innerHTML = formData.horaAlmuerzo;
+    selectedRow.cells[11].innerHTML = formData.montosNegociados;
+    selectedRow.cells[12].innerHTML = formData.comentariosAdicionales;
+    selectedRow.cells[13].innerHTML = formData.evidencia;
+    selectedRow.cells[14].innerHTML = formData.ddSupervisor; 
     selectedRow.cells[15].innerHTML = formData.timestamp;
 }
 
 function onDelete(td) {
 
     row = td.parentElement.parentElement;
-    let timestamp = row.cells[10].innerHTML;
+    let timestamp = row.cells[15].innerHTML;
 
     if (confirm('Are you sure to delete this record ? -> ' + timestamp.toString())) {
 
